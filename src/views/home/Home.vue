c614712f4c60186568067bcea9cc711da75f1be5
IENILOVE <2706925712@qq.com>
2021-08-12 20:00:20 +0800

    首页开发轮播图、推荐信息

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="home">
     <nav-bar class="hoem-navbar"><div slot="center">女装购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend :recommends="recommends"></home-recommend>
   </div>
 </template>
 
 <script>
 
 import NavBar from '../../components/common/navbar/NavBar.vue'
+import HomeSwiper from './childcomp/HomeSwiper.vue'
+import HomeRecommend from './childcomp/HomeRecommend.vue'
+
 
 import {getHomeMultidata} from '../../network/home'
+
 
 
 export default {
     name : 'Home',
     components : {
-      NavBar
+      NavBar,
+      HomeSwiper,
+      HomeRecommend
     },
     data(){
       return{
