964f4cadbcc1a47bc94b145527849434cffa04a7
IENILOVE <2706925712@qq.com>
2021-08-12 11:30:25 +0800

    首页navbar、axios封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,42 @@
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="hoem-navbar"><div slot="center">女装购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
+
+import NavBar from '../../components/common/navbar/NavBar.vue'
+
+import {getHomeMultidata} from '../../network/home'
+
+
 export default {
-    name : 'Home'
+    name : 'Home',
+    components : {
+      NavBar
+    },
+    data(){
+      return{
+        // result : null
+        banners : [],
+        recommends : [],
+      }
+    },
+    created(){
+      getHomeMultidata().then(res =>{
+        // console.log(res)
+        // this.result = res
+        this.banners = res.data.banner.list,
+        this.recommends = res.data.recommend.list
+      })
+    }
 }
 </script>
 
 <style>
-
+  .hoem-navbar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
 </style>
